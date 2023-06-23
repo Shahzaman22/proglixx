c508056bbe83a85865839f6dd8e05e6715272d65
ShahzamanKodexo <shahzaman.aftab@kodexolabs.com>
2023-06-23 17:38:40 +0500

    Testing

--- config/db.js
+++ config/db.js
@@ -1,5 +1,6 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_URL)
-.then(() => console.log('DB is connected to PROGLIX'))
-.catch(() => console.log('Not connected'))
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB is connected to PROGLIX"))
+  .catch((e) => console.log("Not connected", e));
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,108 +1,106 @@
-const {sendEmail} = require('../utils/mailer')
-const bcrypt = require('bcrypt')
-const {User, schema} = require('../model/user')
-const jwt = require('jsonwebtoken')
+const { sendEmail } = require("../utils/mailer");
+const bcrypt = require("bcrypt");
+const { User, schema } = require("../model/user");
+const jwt = require("jsonwebtoken");
 // const {generateOtp}  = require('../utils/generateOtp')
 
-exports.getUser = async(req,res) => {
-   try {
-    const user = await User.find()
-    res.json(user)
-   } catch (error) {
-    console.log(error.message);
-   }
-}
-
-//Generating an OTP 
-exports.createUser = async (req, res) => {
-
-  const {error} = schema.validate(req.body)
-      if (error) return res.status(404).send(error.details[0].message)
-  
-      let user = await User.findOne({ phone: req.body.phone })
-      if (user) return res.status(400).send("User already registered")
-
-  let { name, email, password , phone, role , gender} = req.body;
-
-  const salt = await bcrypt.genSalt(10)
-  password = await bcrypt.hash(password,salt)
-
+exports.getUser = async (req, res) => {
   try {
-    const digits = '0123456789';
-    let otpCode = '';
-    for (let i = 0; i < 6; i++) {
-      otpCode += digits[Math.floor(Math.random() * 10)];
-    }
-
-    const subject = 'OTP code for registration';
-    const text = `Your OTP code is ${otpCode}`;
-
-    await sendEmail(email, subject, text);
-
-    req.session.otpCode = otpCode;
-    req.session.userDetails = {
-      name,
-      email,
-      password,
-      phone,
-      role,
-      gender
-    };
-
-    res.send('Email sent for OTP verification');
+    const user = await User.find();
+    res.json(user);
   } catch (error) {
-    console.error(error);
-    res.status(500).send('Failed to send email');
+    console.log(error.message);
   }
 };
 
-exports.loginUser = async (req,res) => {
+//Generating an OTP
+exports.createUser = async (req, res) => {
+  // const {error} = schema.validate(req.body)
+  //     if (error) return res.status(404).send(error.details[0].message)
 
-    let user = await User.findOne({ name: req.body.name })
-    if (!user) { return res.status(400).send("Invalid name") };
+  //     let user = await User.findOne({ phone: req.body.phone })
+  //     if (user) return res.status(400).send("User already registered")
 
-    let isPassword = await bcrypt.compare(req.body.password, user.password)
-    console.log(req.body.password);
-    console.log(user.password);
-    if (!isPassword) {
-        return res.status(400).send("INVALID PASSWORD")
-    }
+  let { name, email, password, phone, role, gender } = req.body;
 
-   const token = jwt.sign({userId : user._id, userRole : user.role}, process.env.PRIVATE_KEY)
-    return res.json({ token: token, msg: "Login Successfully" })
+  const salt = await bcrypt.genSalt(10);
+  hashPassword = await bcrypt.hash(password, salt);
 
+  try {
+    // const digits = '0123456789';
+    // let otpCode = '';
+    // for (let i = 0; i < 6; i++) {
+    //   otpCode += digits[Math.floor(Math.random() * 10)];
+    // }
+
+    // const subject = 'OTP code for registration';
+    // const text = `Your OTP code is ${otpCode}`;
+
+    // await sendEmail(email, subject, text);
+
+    // req.session.otpCode = otpCode;
+    // req.session.userDetails = {
+    //   name,
+    //   email,
+    //   password,
+    //   phone,
+    //   role,
+    //   gender
+    // };
+    const user = await User.create({ name, email, password: hashPassword });
+
+    res.json({ message: "created Successfully", user });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Failed to send email");
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  let user = await User.findOne({ name: req.body.name });
+  if (!user) {
+    return res.status(400).send("Invalid name");
+  }
+
+  let isPassword = await bcrypt.compare(req.body.password, user.password);
+  console.log(req.body.password);
+  console.log(user.password);
+  if (!isPassword) {
+    return res.status(400).send("INVALID PASSWORD");
+  }
+
+  const token = jwt.sign(
+    { userId: user._id, userRole: user.role },
+    process.env.PRIVATE_KEY
+  );
+  return res.json({ token: token, msg: "Login Successfully" });
 };
 
 exports.updateUser = async (req, res) => {
-    try {
-      const updatedUser = await User.updateMany(
-        { phone: req.body.phone },
-        { $set: (req.body) }
-      );
-      if (!updatedUser) {
-        return res.status(400).send('User not found');
-      }
-  
-      res.json(updatedUser);
-
-} catch (err) {
-      console.error(err);
-      res.status(500).send('Server Error');
+  try {
+    const updatedUser = await User.updateMany(
+      { phone: req.body.phone },
+      { $set: req.body }
+    );
+    if (!updatedUser) {
+      return res.status(400).send("User not found");
     }
+
+    res.json(updatedUser);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server Error");
+  }
 };
 
-exports.deleteUser = async (req,res) => {
- 
+exports.deleteUser = async (req, res) => {
+  const dltUser = await User.deleteOne({ phone: req.body.phone });
+  if (!dltUser) {
+    return res.status(400).send("User not found");
+  }
 
-    const dltUser = await User.deleteOne(
-        { phone: req.body.phone }
-      );
-      if (!dltUser) {
-        return res.status(400).send('User not found');
-      }
-
-   res.json({user : dltUser , msg : "Deleted Successfully"})
-}
+  res.json({ user: dltUser, msg: "Deleted Successfully" });
+};
 
 //Verifying Otp and creating User
 exports.verifyOtpAndCreateUser = async (req, res) => {
@@ -121,32 +119,31 @@ exports.verifyOtpAndCreateUser = async (req, res) => {
       res.json(user);
     } catch (error) {
       console.error(error);
-      res.status(500).send('Failed to create user');
+      res.status(500).send("Failed to create user");
     }
   } else {
-    res.status(400).send('Invalid OTP');
+    res.status(400).send("Invalid OTP");
   }
 };
 
 exports.editUser = async (req, res) => {
   const user = await User.findByIdAndUpdate(req.user.userId, req.body);
   // console.log(req.user);
-  if (!user) return res.status(403).send('User not found')
+  if (!user) return res.status(403).send("User not found");
 
-  let isPassword = await bcrypt.compare(req.body.password, user.password)
+  let isPassword = await bcrypt.compare(req.body.password, user.password);
   // console.log("REQ =>", req.body.password);
   // console.log("USER =>", user.password);
   if (isPassword) {
-    return res.status(400).send("INVALID PASSWORD")
+    return res.status(400).send("INVALID PASSWORD");
   }
 
   // check if password is updated
   if (req.body.password) {
-    const salt = await bcrypt.genSalt(10)
-    user.password = await bcrypt.hash(req.body.password, salt)
-    await user.save()
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(req.body.password, salt);
+    await user.save();
   }
 
   res.send(user);
-}
-
+};
